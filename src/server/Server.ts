8c61c34e4542c470bbafc0ed85e2710c223d9565
Natanna99 <erzafairy79@gmail.com>
2024-08-27 19:06:51 -0300

    Chore: add forma de visualizar info do body e removendo rota get para arquivo routes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,15 @@
 //Codigo base do servidor
 import express from "express";
 
+// Importando o router
+import { router } from "../routers";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World Dev!");
-});
+// Configuração de middleware - para exibir dados do body das requisições
+app.use(express.json());
+
+// Configuração de middleware - para realizar a chmada das rotas do routers
+app.use(router);
 
 export { app };
