e6d3ad2d142844d8aabc9e021848917f6af6ce29
Natanna99 <erzafairy79@gmail.com>
2024-08-30 17:32:56 -0300

    Chore: criacao da constante de validacao passandos varios tipos de request e seus esquemas

--- src/routers/CitiesRoutes.ts
+++ src/routers/CitiesRoutes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { create } from "../controllers/Cities/CreateCityController";
+import { validator } from "../shared/middlewares/Validator";
+import { schemaCreateCity, schemaFilterCity } from "../schema/CitiesSchame";
 
 const CitiesRouter = Router();
 
@@ -7,6 +9,13 @@ CitiesRouter.get("/", (req, res) => {
   res.send("Hello World Dev!");
 });
 
-CitiesRouter.post("/create", create);
+//Contantes para validação de corpo de requisição com o tipo de validação (body) recebendo o esquema de validação para criação de cidade
+const createValidator = validator({
+  body: schemaCreateCity,
+  query: schemaFilterCity,
+});
+
+//Rota de criação de cidade (validar o corpo da chamada com o middleware createBodyValidator, depois chamar o método create)
+CitiesRouter.post("/create", createValidator, create);
 
 export { CitiesRouter };
